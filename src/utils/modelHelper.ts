9a0691a33ef4fea1b9447fa2786daa83222178a8
Kemal Erdem <kemalpiro@gmail.com>
2023-02-19 13:18:05 +0100

    init moduleHelper

--- src/utils/modelHelper.ts
+++ src/utils/modelHelper.ts
@@ -1,9 +1,27 @@
-import * as ort from "onnxruntime-web";
-import {InferenceSession} from "onnxruntime-web";
+import * as tf from '@tensorflow/tfjs-core';
+import {createDetector, movenet, PoseDetector, SupportedModels} from "./pose-detection";
+import {STATE} from "./pose-detection/params";
 
-export async function initializeModel(): Promise<InferenceSession> {
-  console.log(ort)
-  const model = await ort.InferenceSession.create("./hrnet512.onnx");
+export async function initializeModel(): Promise<PoseDetector> {
+  return createDetector(SupportedModels.MoveNet, {modelType: movenet.modelType.SINGLEPOSE_THUNDER});
+}
 
-  return model;
+export async function setBackendAndEnvFlags(flagConfig: typeof STATE['flags'], backend: SupportedModels) {
+  tf.env().setFlags(flagConfig);
+
+  const [runtime, backendName] = backend.split('-');
+
+  if (runtime === 'tfjs') {
+    const ENGINE = tf.engine();
+    if (!(backendName in ENGINE.registryFactory)) {
+      throw new Error(`${backendName} backend is not registered.`);
+    }
+    if (backendName in ENGINE.registry) {
+      const backendFactory = tf.findBackendFactory(backendName);
+      tf.removeBackend(backendName);
+      tf.registerBackend(backendName, backendFactory);
+    }
+
+    await tf.setBackend(backendName);
+  }
 }
