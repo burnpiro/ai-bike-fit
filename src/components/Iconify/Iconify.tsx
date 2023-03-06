c56a54b4b43a2d0d409318f949644b9843c0c988
Kemal Erdem <kemalpiro@gmail.com>
2023-03-06 23:20:27 +0100

    custom icons

---
+++ src/components/Iconify/Iconify.tsx
@@ -0,0 +1,26 @@
+import { forwardRef } from "react";
+// icons
+import { Icon } from "@iconify/react";
+// @mui
+import { Box, BoxProps } from "@mui/material";
+import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+
+// ----------------------------------------------------------------------
+
+interface IconifyProps extends BoxProps {
+  icon: string | ReactJSXElement;
+}
+
+const Iconify = forwardRef(
+  ({ icon, width = 20, sx, ...other }: IconifyProps, ref) => (
+    <Box
+      ref={ref}
+      component={Icon}
+      icon={icon}
+      sx={{ width, height: width, ...sx }}
+      {...other}
+    />
+  )
+);
+
+export default Iconify;
