9510ef8b5db40237ce4d9771d47e7931f3859bab
Kemal Erdem <kemalpiro@gmail.com>
2023-03-08 23:23:28 +0100

    create custom MUI theme

---
+++ src/theme/index.tsx
@@ -0,0 +1,55 @@
+import {PropsWithChildren, useMemo} from 'react';
+// @mui
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider as MUIThemeProvider, createTheme, StyledEngineProvider } from '@mui/material';
+//
+import palette from './palette';
+import shadows from './shadows';
+import typography from './typograpthy';
+import GlobalStyles from './globalStyles';
+import customShadows, {CustomShadows} from './customShadows';
+import componentsOverride from './overrides';
+import {ThemeOptions} from "@mui/material/styles/createTheme";
+
+interface ThemeProviderProps {
+
+}
+
+export default function ThemeProvider({ children }: PropsWithChildren<ThemeProviderProps>) {
+    const themeOptions = useMemo<ThemeOptions>(
+        () => ({
+            palette,
+            shape: { borderRadius: 6 },
+            typography,
+            shadows: shadows(),
+            customShadows: customShadows(),
+        }),
+        []
+    );
+
+    const theme = createTheme(themeOptions);
+    theme.components = componentsOverride(theme);
+
+    return (
+        <StyledEngineProvider injectFirst>
+            <MUIThemeProvider theme={theme}>
+                <CssBaseline />
+                <GlobalStyles />
+                {children}
+            </MUIThemeProvider>
+        </StyledEngineProvider>
+    );
+}
+
+declare module '@mui/material/styles' {
+    interface Theme {
+        customShadows: CustomShadows;
+    }
+    interface TypeBackground {
+        neutral: string;
+    }
+    // allow configuration using `createTheme`
+    interface ThemeOptions {
+        customShadows: CustomShadows;
+    }
+}
