435cfad6536b1a04bcb2d5d8a0fe318e47fbcda4
Kemal Erdem <kemalpiro@gmail.com>
2023-03-19 19:49:55 +0100

    Add layouts and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,23 @@
-import React, {useEffect, useState} from 'react';
-import '@tensorflow/tfjs-backend-webgl';
-import logo from './logo.svg';
-import './App.css';
-import {initializeModel, setBackendAndEnvFlags} from "./utils/modelHelper";
-import {STATE} from "./utils/pose-detection/params";
-import {createDetector, PoseDetector, SupportedModels} from "./utils/pose-detection";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+// routes
+import { Router } from "./routes";
+// theme
+import ThemeProvider from "./theme";
+// components
+import ScrollToTop from "./components/ScrollToTop";
 
-function App() {
-  const [detector, setDetector] = useState<PoseDetector | null>(null);
+// ----------------------------------------------------------------------
 
-  useEffect(() => {
-    const setupTF = async () => {
-      console.log('setting TF');
-      await setBackendAndEnvFlags(STATE.flags, STATE.backend as SupportedModels);
-
-      const newDetector = await createDetector(STATE.model, STATE.modelConfig);
-
-      setDetector(newDetector);
-    }
-
-    setupTF()
-  }, [])
-
-  console.log(detector);
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <ScrollToTop />
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
-
-export default App;
