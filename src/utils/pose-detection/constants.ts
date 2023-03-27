59349b40f8e2573cb120d99a43e4511d466d49cf
Kemal Erdem <kemalpiro@gmail.com>
2023-03-28 00:46:54 +0200

    Add dict with keypoint names

--- src/utils/pose-detection/constants.ts
+++ src/utils/pose-detection/constants.ts
@@ -17,17 +17,64 @@
 // Don't change the order. The order needs to be consistent with the model
 // keypoint result list.
 export const COCO_KEYPOINTS = [
-  'nose', 'left_eye', 'right_eye', 'left_ear', 'right_ear', 'left_shoulder',
-  'right_shoulder', 'left_elbow', 'right_elbow', 'left_wrist', 'right_wrist',
-  'left_hip', 'right_hip', 'left_knee', 'right_knee', 'left_ankle',
-  'right_ankle'
+  "nose",
+  "left_eye",
+  "right_eye",
+  "left_ear",
+  "right_ear",
+  "left_shoulder",
+  "right_shoulder",
+  "left_elbow",
+  "right_elbow",
+  "left_wrist",
+  "right_wrist",
+  "left_hip",
+  "right_hip",
+  "left_knee",
+  "right_knee",
+  "left_ankle",
+  "right_ankle",
 ];
+
+export const KEYPOINT_NAMES = {
+  NOSE: "nose",
+  LEFT_EYE: "left_eye",
+  RIGHT_EYE: "right_eye",
+  LEFT_EAR: "left_ear",
+  RIGHT_EAR: "right_ear",
+  LEFT_SHOULDER: "left_shoulder",
+  RIGHT_SHOULDER: "right_shoulder",
+  LEFT_ELBOW: "left_elbow",
+  RIGHT_ELBOW: "right_elbow",
+  LEFT_WRIST: "left_wrist",
+  RIGHT_WRIST: "right_wrist",
+  LEFT_HIP: "left_hip",
+  RIGHT_HIP: "right_hip",
+  LEFT_KNEE: "left_knee",
+  RIGHT_KNEE: "right_knee",
+  LEFT_ANKLE: "left_ankle",
+  RIGHT_ANKLE: "right_ankle",
+};
 export const COCO_KEYPOINTS_BY_SIDE = {
   left: [1, 3, 5, 7, 9, 11, 13, 15],
   right: [2, 4, 6, 8, 10, 12, 14, 16],
-  middle: [0]
+  middle: [0],
 };
 export const COCO_CONNECTED_KEYPOINTS_PAIRS = [
-  [0, 1], [0, 2], [1, 3], [2, 4], [5, 6], [5, 7], [5, 11], [6, 8], [6, 12],
-  [7, 9], [8, 10], [11, 12], [11, 13], [12, 14], [13, 15], [14, 16]
+  [0, 1],
+  [0, 2],
+  [1, 3],
+  [2, 4],
+  [5, 6],
+  [5, 7],
+  [5, 11],
+  [6, 8],
+  [6, 12],
+  [7, 9],
+  [8, 10],
+  [11, 12],
+  [11, 13],
+  [12, 14],
+  [13, 15],
+  [14, 16],
 ];
