dbb2b9fa38b87629c85cf7e735322c3ac61a211c
Kemal Erdem <kemalpiro@gmail.com>
2023-03-30 00:50:35 +0200

    Add 4th step to record session process

--- src/pages/RecordSession/RecordSessionPage.tsx
+++ src/pages/RecordSession/RecordSessionPage.tsx
@@ -1,18 +1,12 @@
 import React, { Fragment, useEffect, useState } from "react";
-import useDeviceOrientation from "../../hooks/useDeviceOrientation";
 import CameraVerticalPositionSetupPage from "./CameraVerticalPositionSetupPage";
 import CameraHorizontalPositionSetupPage from "./CameraHorizontalPositionSetupPage";
 import RecordVideoPage from "./RecordVideoPage";
+import ProcessRecordingPage from "./ProcessRecording";
 
 function RecordSessionPage() {
-  const orientation = useDeviceOrientation();
   const [step, setStep] = useState(0);
-
-  // console.log(detector);
-  const isLandscape =
-    orientation.beta == null ||
-    Math.abs(orientation.beta) < 20 ||
-    Math.abs(orientation.beta) > 160;
+  const [recordingData, setRecordingData] = useState<Blob[] | null>(null);
 
   const handleNextPage = () => {
     setStep(step + 1);
@@ -21,21 +15,25 @@ function RecordSessionPage() {
     setStep(step > 0 ? step - 1 : 0);
   };
 
+  const handleSetRecording = (newRecordingData: Blob[]) => {
+    setRecordingData(newRecordingData);
+    setStep(3);
+  };
+
   return (
     <Fragment>
       {step === 0 && (
         <CameraHorizontalPositionSetupPage onNext={handleNextPage} />
       )}
       {step === 1 && (
-          <CameraVerticalPositionSetupPage
-              onNext={handleNextPage}
-              onPrev={handlePrevPage}
-          />
+        <CameraVerticalPositionSetupPage
+          onNext={handleNextPage}
+          onPrev={handlePrevPage}
+        />
       )}
-      {step === 2 && (
-          <RecordVideoPage
-              onPrev={handlePrevPage}
-          />
+      {step === 2 && <RecordVideoPage onNext={handleSetRecording} />}
+      {step === 3 && recordingData && (
+        <ProcessRecordingPage recording={recordingData} />
       )}
     </Fragment>
   );
