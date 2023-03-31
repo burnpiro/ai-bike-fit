4c49d72a9e1ccd878feb99a45a78106e4b2a07c5
Kemal Erdem <kemalpiro@gmail.com>
2023-03-31 21:06:04 +0200

    Use Vite instead of CRA

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    let vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
+});
---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    manifest: true,
+  },
+  server: {
+    port: 3000,
+    host: true,
+    https: {
+      key: "./reactcert/key.pem",
+      cert: "./reactcert/cert.pem",
+    },
+  },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'logo192.png', 'logo.svg'],
+      manifest: {
+        name: 'AI Bike Fit',
+        short_name: 'AIBikeFit',
+        description: 'Bike Fitting with the help of AI',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+    react(),
+  ],
+});
