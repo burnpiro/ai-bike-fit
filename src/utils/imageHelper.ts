4b0f660b51fd1125fbc460a61a83f29546a0a128
Kemal Erdem <kemalpiro@gmail.com>
2023-04-03 15:42:10 +0200

    Create image extractor base onf recording blog and frameTime

--- src/utils/imageHelper.ts
+++ src/utils/imageHelper.ts
@@ -1,3 +1,38 @@
-export function imageHelper() {
+export async function extractFrameFromRecording(
+  recording: Blob,
+  frameTime: number
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    try {
+      const videoUrl = URL.createObjectURL(recording);
+      const videoElement = document.createElement("video");
+      videoElement.addEventListener("loadedmetadata", () => {
+        const canvas = document.createElement("canvas");
+        const width = videoElement.videoWidth;
+        const height = videoElement.videoHeight;
+        canvas.width = width;
+        canvas.height = height;
 
+        const ctx = canvas.getContext("2d");
+        if (!ctx) {
+          return;
+        }
+
+        videoElement.currentTime = frameTime;
+
+        const render = () => {
+          ctx.drawImage(videoElement, 0, 0, width, height);
+          const imageDataURL = canvas.toDataURL();
+          resolve(imageDataURL);
+
+          videoElement.removeEventListener("timeupdate", render);
+        };
+        videoElement.addEventListener("timeupdate", render);
+      });
+      videoElement.src = videoUrl;
+      videoElement.load();
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
